49e81e51a39d9f8cda99633a4ed619fd276bae0e
alias <aliashko@ukr.net>
2022-10-21 11:53:11 +0300

    create project and added reading function

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import fileManager as fm
+
+import container_Manager as cm
+file_dir = "D:/holter_files/2@1951-01-31.edf"
+n=cm.Container(file_dir)
+cont=""
+n.filling_containers(cont)
+# cm.container_filling(1, 1)
+
+
