ed6bfc5f3822ed0f38a2e628f777bcd4d618068b
alias <aliashko@ukr.net>
2023-01-26 00:59:56 +0200

    this file start our program

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import StaticMethods
-import fileManager as fm
-
-import container_Manager as cm
+import graph_Manager
 import graph_Manager as gm
 
-file_dir = "D:/holter_files/2@1951-01-31.edf"
-n = cm.Container(file_dir)
+gm = graph_Manager.start_plot()
 
-inp_list = []
-time_list = []
-n.writeFileToList1(inp_list, time_list, 0)
-gm.animate(time_list, inp_list, 60, 120)
-
-gm.animate(time_list, inp_list, 0, 60)
-# cm.container_filling(1, 1)
-# print(StaticMethods.MainMethods.predictionLimits(inp_list, 4))
