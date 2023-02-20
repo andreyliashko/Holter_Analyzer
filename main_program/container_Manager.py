e0c82fc4e1bbb6c7497fff5339dc30db402ceb87
alias <aliashko@ukr.net>
2023-02-20 12:29:58 +0200

    Modified class. Now we can save needed segment to files

--- main_program/container_Manager.py
+++ main_program/container_Manager.py
@@ -1,8 +1,7 @@
 import os
 
+from start_module import Variables
 import fileManager
-import scipy.stats as stats
-import numpy as np
 
 
 class Container:
@@ -14,7 +13,7 @@ class Container:
     def __init__(self, directory):
         self.dir = directory
         self.current_status = 0
-        self.delta = 10
+        self.delta = Variables.FilesConstant.get_points_in_one_sec
         self.filling_containers()
         self.points_number = 0
         self.time_duration = 0
@@ -41,24 +40,45 @@ class Container:
     def getSecondsToTime(time_sec, points_in_sec):
         return str(time_sec / points_in_sec)
 
-    def writeFileToList1(self, output_list, out_date, file_number=0):
+    def writeFileToListAndDate(self, output_list0, out_date, file_number):
 
         if not self.current_status:
             return 0
-        reading_path = self.dir + fileManager.ending + str(file_number) + fileManager.file_type
+        reading_path = self.dir + fileManager.ending + str(file_number) + Variables.FilesConstant.text_type
         check_file = os.path.isfile(reading_path)
         if check_file:
             inp_file = open(reading_path)
-            j = 0
+            j = -1
             self.fill_input_param(inp_file.readline())
             point_in_second = (self.getPointsAmount() / self.time_duration)
             for i in inp_file:
                 j = j + 1
                 if j % self.delta == 0:
                     out_date.append(float(self.getSecondsToTime(j, point_in_second).strip()))
-                    output_list.append(float(i.strip()))
+                    output_list0.append(float(i.strip()))
             inp_file.close()
-            print("container successfully filled...")
+            print("container "+str(file_number)+" successfully filled...")
+            return 1
+        print("An error occurred")
+        return 0
+
+    def writeFileToList(self, output_list0, file_number):
+
+        if not self.current_status:
+            return 0
+        reading_path = self.dir + fileManager.ending + str(file_number) + Variables.FilesConstant.text_type
+        check_file = os.path.isfile(reading_path)
+        if check_file:
+            inp_file = open(reading_path)
+            j = -1
+            self.fill_input_param(inp_file.readline())
+            point_in_second = (self.getPointsAmount() / self.time_duration)
+            for i in inp_file:
+                j = j + 1
+                if j % self.delta == 0:
+                    output_list0.append(float(i.strip()))
+            inp_file.close()
+            print("container " + str(file_number) + " successfully filled...")
             return 1
         print("An error occurred")
         return 0
