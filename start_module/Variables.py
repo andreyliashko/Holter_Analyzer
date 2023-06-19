067f96e2df87929090f6d47b6aaef4b5f6894c67
alias <aliashko@ukr.net>
2023-06-19 18:44:35 +0300

    add new variables to save deleted sections

--- start_module/Variables.py
+++ start_module/Variables.py
@@ -1,6 +1,6 @@
 class FilesConstant:
-    # file_directory = "D:/holter_files/2@1951-01-31.edf"
-    file_directory = "D:/holter_files/3@f@1953-10-07.edf"
+    file_directory = "D:/holter_files/2@1951-01-31.edf"
+    # file_directory = "D:/holter_files/3@f@1953-10-07.edf"
     current_signal = 1
     get_points_in_one_sec = 10
     screen_type = ".pdf"
@@ -10,3 +10,9 @@ class FilesConstant:
 class GraphConstant:
     delta_time = 60
     gen_files_dir = "D:/holter_files/file"
+
+
+class autoDeleteGraphConstant:
+    where_to_save_file_direction = "D:/holter_files/auto_delete"
+    save_from_time = "13.11.1999"
+    save_to_time = "14.11.1999"
